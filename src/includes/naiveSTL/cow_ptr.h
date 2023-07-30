a758a1b17541c179a088a39dff06da715638eb18
apple zhang <zhangshe@usc.edu>
2023-07-30 01:55:24 -0700

    work on cow pointer

--- src/includes/naiveSTL/cow_ptr.h
+++ src/includes/naiveSTL/cow_ptr.h
@@ -12,25 +12,25 @@ namespace NaiveSTL {
     template<class T>
     class cow_ptr;
 
-    namespace Impl{
+    namespace Impl {
         template<class T>
-        class proxy{
+        class proxy {
         public:
-            explicit proxy(cow_ptr<T> *const cp): cp_(cp) {}
-            proxy(const proxy& rhs) = default;
-            proxy& operator = (const proxy& rhs) = default;
+            explicit proxy(cow_ptr<T> *const cp) : cp_(cp) {}
 
-            const T& operator * () const;
+            proxy(const proxy &rhs) = default;
 
-            T& operator * ();
+            proxy &operator=(const proxy &rhs) = default;
 
-            const T *operator -> () const;
+            const T &operator*() const;
 
-            T *operator -> ();
+            T &operator*();
 
-            cow_ptr<T> &operator=(const T &rhs);
+            const T *operator->() const;
 
-            operator T() const;
+            T *operator->();
+
+            explicit operator T() const;
 
         private:
             cow_ptr<T> *const cp_;
@@ -44,14 +44,15 @@ namespace NaiveSTL {
     public:
         typedef T element_type;
     private:
-        template<class _T>
-                using proxy = Impl::proxy<_T>;
+        template<class C>
+        using proxy = Impl::proxy<C>;
 
     public:
         explicit cow_ptr(T *p = nullptr);
 
         template<class D>
-                cow_ptr(T *p, D d);
+                requires Concept::Deleter<D, T>
+        cow_ptr(T *p, D d);
 
         cow_ptr(const cow_ptr &cp);
 
@@ -59,9 +60,9 @@ namespace NaiveSTL {
 
         const proxy<element_type>& operator*() const;
 
-        proxy<element_type>& operator*();
+        proxy<element_type> operator*();
 
-        const proxy<element_type> operator->() const;
+        proxy<element_type> operator->() const;
 
         proxy<element_type> operator->();
 
@@ -69,12 +70,105 @@ namespace NaiveSTL {
 
         const element_type *get() const;
 
-        operator bool() const;
 
     private:
         shared_ptr<element_type> ptr_;
 
+    public:
+
+
     };
+
+    template<class T1, class ... Args>
+    cow_ptr<T1> make_cow(Args &&... args) {
+        return cow_ptr<T1>(new T1(std::forward<Args>(args)...));
+    }
+
+    namespace Impl {
+        template<class T>
+        const T &proxy<T>::operator*() const {
+            return *(cp_->ptr_);
+        }
+
+        template<class T>
+        T &proxy<T>::operator*() {
+            auto t = *(cp_->ptr_);
+            cp_->ptr_ = make_shared<T>(t);
+            return *(cp_->ptr_);
+        }
+
+        template<class T>
+        const T *proxy<T>::operator->() const {
+            return cp_->ptr_.get();
+        }
+
+        template<class T>
+        T *proxy<T>::operator->() {
+            auto t = *(cp_->ptr_);
+            cp_->ptr_ = make_shared<T>(t);
+            return cp_->ptr_.get();
+        }
+
+
+        template<class T>
+        proxy<T>::operator T() const {
+            return *(cp_->ptr_);
+        }
+
+
+    }
+    template<class T>
+    cow_ptr<T>::cow_ptr(T *p) : ptr_(p) {}
+
+    template<class T>
+    template<class D>
+    requires Concept::Deleter<D, T>
+    cow_ptr<T>::cow_ptr(T *p, D d) : ptr_(p, d) {}
+
+    template<class T>
+    cow_ptr<T>::cow_ptr(const cow_ptr &cp) : ptr_(cp.ptr_) {}
+
+    template<class T>
+    cow_ptr<T> &cow_ptr<T>::operator=(const cow_ptr &cp) {
+        if(this != &cp){
+            ptr_ = cp.ptr_;
+        }
+
+        return *this;
+    }
+
+    template<class T>
+    typename cow_ptr<T>::element_type *cow_ptr<T>::get() {
+        return ptr_.get();
+    }
+
+    template<class T>
+    const typename cow_ptr<T>::element_type* cow_ptr<T>::get() const{
+        return ptr_.get();
+    }
+
+
+    template<class T>
+    const typename cow_ptr<T>::template proxy<T>& cow_ptr<T>::operator*() const {
+        return proxy<T>(const_cast<cow_ptr<T> *>(this));
+    }
+
+    template<class T>
+    typename cow_ptr<T>:: template proxy<T> cow_ptr<T>::operator ->(){
+        return proxy<T>(this);
+    }
+
+    template<class T>
+    typename cow_ptr<T>:: template proxy<T> cow_ptr<T>::operator *(){
+        return proxy<T>(this);
+    }
+
+    template<class T>
+     typename cow_ptr<T>:: template proxy<T> cow_ptr<T>::operator ->()const{
+        return proxy<T>(const_cast<cow_ptr *const>(this));
+    }
+
 }
+
 
 #endif //NAIVESTL_COW_PTR_H
